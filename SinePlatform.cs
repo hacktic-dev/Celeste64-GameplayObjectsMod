f66af2c802334d0ffdc1f121ab6a570dd48eadac
hacktic_ <foorganders@outlook.com>
2024-02-01 21:36:00 +0100

    Initial commit

---
+++ SinePlatform.cs
@@ -0,0 +1,43 @@
+﻿using Celeste64;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinePlatformMod
+{
+	internal class SinePlatform : Solid
+	{
+
+		internal SinePlatform(float phase, string direction) { m_phase = phase; m_direction = direction; }
+
+		float m_phase;
+		float m_time;
+		string m_direction;
+		float m_multiplier = 0.5f;
+
+		public override void Update()
+		{
+			base.Update();
+
+			m_time += Foster.Framework.Time.Delta;
+
+			var offset = new System.Numerics.Vector3(0, 0, 0);
+
+			if (m_direction == "X")
+				offset = new System.Numerics.Vector3(m_multiplier * MathF.Cos(m_time + m_phase), 0, 0);
+			else if (m_direction == "Y")
+				offset = new System.Numerics.Vector3(0, m_multiplier * MathF.Cos(m_time + m_phase), 0);
+			else
+				offset = new System.Numerics.Vector3(0, 0, m_multiplier * MathF.Cos(m_time + m_phase));
+
+			Position += offset;
+			if (HasPlayerRider())
+			{
+				World.Get<Player>().Position += offset;
+			}
+		}
+
+	}
+}
---
+++ SinePlatformMod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Celeste64;
+namespace SinePlatformMod
+{
+    public class SinePlatformMod : GameMod
+    {
+        public override void OnModLoaded()
+        {
+            base.OnModLoaded();
+
+						Map.ActorFactory factory = new((map, entity) => new SinePlatform(entity.GetFloatProperty("phase", 0), entity.GetStringProperty("direction", "X"))) { IsSolidGeometry = true };
+						AddActorFactory("SinePlatform", factory);
+        }
+    }
+}
