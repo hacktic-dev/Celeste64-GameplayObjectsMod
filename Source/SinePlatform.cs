1abf597bd4cdb3bfb595d3e3ec8d2c328f402bb5
hacktic_ <foorganders@outlook.com>
2024-02-02 14:40:08 +0100

    Make sine platforms use global time

--- Source/SinePlatform.cs
+++ Source/SinePlatform.cs
@@ -7,30 +7,29 @@ using System.Threading.Tasks;
 
 namespace PlatformsMod
 {
-	internal class SinePlatform : Solid
+	internal class SinePlatform : Solid, IGlobalTime
 	{
 
 		internal SinePlatform(float phase, string direction) { m_phase = phase; m_direction = direction; }
 
 		float m_phase;
-		float m_time;
 		string m_direction;
 		float m_multiplier = 0.5f;
+
+		public float Time { get; set; }
 
 		public override void Update()
 		{
 			base.Update();
 
-			m_time += Foster.Framework.Time.Delta;
-
 			var offset = new System.Numerics.Vector3(0, 0, 0);
 
 			if (m_direction == "X")
-				offset = new System.Numerics.Vector3(m_multiplier * MathF.Cos(m_time + m_phase), 0, 0);
+				offset = new System.Numerics.Vector3(m_multiplier * MathF.Cos(Time + m_phase), 0, 0);
 			else if (m_direction == "Y")
-				offset = new System.Numerics.Vector3(0, m_multiplier * MathF.Cos(m_time + m_phase), 0);
+				offset = new System.Numerics.Vector3(0, m_multiplier * MathF.Cos(Time + m_phase), 0);
 			else
-				offset = new System.Numerics.Vector3(0, 0, m_multiplier * MathF.Cos(m_time + m_phase));
+				offset = new System.Numerics.Vector3(0, 0, m_multiplier * MathF.Cos(Time + m_phase));
 
 			Position += offset;
 			if (HasPlayerRider())
